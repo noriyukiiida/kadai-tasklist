22e4de1803aa961b7d38b29be0a21d1993935c13
EC2 Default User <ec2-user@ip-172-31-46-44.us-east-2.compute.internal>
2018-03-27 08:18:38 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,32 +1,35 @@
 class TasksController < ApplicationController
+  
   def index
-    @task = task.all
+    @tasks = Task.all
+  end
+  
   def show
-    @task = task.find(params[:id])
-    def new
-    @task = task.new
-    def create
-      
-        # Strong Parameter
-  def tasks_params
-    params.require(:task).permit(:content)
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
     
-    @task = task.new(task_params)
+  def create
+    @task = Task.new(task_params)
+
     if @task.save
-      flash[:success] = 'task が正常に投稿されました'
+      flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'task が投稿されませんでした'
+      flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
+  end
 
   def edit
-    @task = task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
-    @task = task.find(params[:id])
-
+    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = 'task は正常に更新されました'
       redirect_to @task
@@ -37,7 +40,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = taske.find(params[:id])
+    @task = Taske.find(params[:id])
     @task.destroy
 
     flash[:success] = 'task は正常に削除されました'
@@ -50,3 +53,4 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content)
   end
+end
