c81f70adc30f76575006558192d4c4b3f9808f15
EC2 Default User <ec2-user@ip-172-31-46-44.us-east-2.compute.internal>
2018-03-28 08:16:05 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328054133) do
+ActiveRecord::Schema.define(version: 20180328080501) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
