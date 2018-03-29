c5ec87f709fd091d70084d57ab781be69148c33e
EC2 Default User <ec2-user@ip-172-31-46-44.us-east-2.compute.internal>
2018-03-29 09:11:01 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,6 +47,13 @@ class TasksController < ApplicationController
   end
 
   private
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 
   def set_task
     @task = Task.find(params[:id])
