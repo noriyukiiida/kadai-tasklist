473226995185ff75402e59323e5dcef55833a7a9
EC2 Default User <ec2-user@ip-172-31-46-44.us-east-2.compute.internal>
2018-03-29 07:34:02 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
    def index
     @users = User.all.page(params[:page])
-  end
+   end
 
   def show
     @user = User.find(params[:id])
+    @tasks = @user.tasks.order('created_at DESC').page(params[:page])
+    counts(@user)
   end
 
   def new
